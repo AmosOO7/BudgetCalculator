c534b4df015031b5b8555cd609714308d50d9965
AmosOO7 <ayokunene@gmail.com>
2024-12-12 20:43:21 +0100

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import "../../global.css";
+
+export default function HomeScreen() {
+  return (
+    <View className="m-11" style={{ backgroundColor: "#fff", flex: 1 }}>
+      <Text>Hello There!!!</Text>
+    </View>
+  );
+}
